ce4a77096fc71ebb25217a980151142057a76e32
Casimir Wallwitz <casimir.wallwitz@tum.de>
2025-11-22 20:50:23 +0100

    add spread strategy

--- utils.py
+++ utils.py
@@ -1,5 +1,8 @@
+import requests
 import statistics as st
 import pandas as pd
+from imcity_template import BaseBot, Side, OrderRequest, OrderBook, Order
+from typing import Tuple
 
 def eisbach_value(flow_rate: float, water_level: float) -> float:
     """
@@ -32,7 +35,7 @@ def weather_3_value(temperatures: list[float]|pd.Series, humidities: list[float]
     # TODO: get half-hourly data and remove *2
     return abs(total_sum*2)
 
-def weather_4_value(temperatures: list[float], humidities: list[float]) -> float:
+def weather_4_value(temperatures: list[float]|pd.Series, humidities: list[float]|pd.Series) -> float:
     """
     Settlement value for 4_Weather
     """
@@ -55,10 +58,12 @@ def weather_4_value(temperatures: list[float], humidities: list[float]) -> float
         h_median = st.median(window_hums)
         
         current_temp = temperatures[i]
+        current_humidity = humidities[i]
         
-        step_value = current_temp * (t_mean - t_median) * (h_mean - h_median)
+        step_value = (current_temp+current_humidity) * ((t_mean - t_median) * (h_mean - h_median))
         
-        total_settlement += step_value
+        # TODO: get half-hourly data and remove *2
+        total_settlement += step_value*2
 
     # 4. Final Absolute Value
     return abs(total_settlement)
@@ -112,3 +117,87 @@ def etf_strangle_value(flow_rate: float, water_level: float, latest_temp: float,
     
     # 3. Total Settlement
     return put_payoff + call_payoff
+
+def get_spread(recent_trades: pd.DataFrame) -> float:
+    # compute the spread
+    ask_trades = recent_trades[recent_trades['aggressor'] == recent_trades['buyer']]
+    bid_trades = recent_trades[recent_trades['aggressor'] == recent_trades['seller']]
+
+    # 2. Calculate the average price for each side
+    avg_ask_price = ask_trades['price'].mean()
+    avg_bid_price = bid_trades['price'].mean()
+
+    # 3. Calculate the Spread
+    if pd.notna(avg_ask_price) and pd.notna(avg_bid_price):
+        average_spread = avg_ask_price - avg_bid_price
+        print(f"Average Ask Price (Buy Aggressor): {avg_ask_price:.2f}")
+        print(f"Average Bid Price (Sell Aggressor): {avg_bid_price:.2f}")
+        print(f"Estimated Spread: {average_spread:.2f}")
+    else:
+        print("Not enough data to calculate spread (need both buy and sell aggressors in the window).")
+    
+    return average_spread
+
+def get_most_traded_VWAP(exchange, username, password) -> Tuple[str, float, float]:
+    class CustomBot(BaseBot):
+        # Handler for own trades
+        def on_trades(self, trades: list[dict]):
+            for trade in trades:
+                print(f"{trade['volume']} @ {trade['price']}")
+
+        # Handler for order book updates
+        def on_orderbook(self, orderbook: OrderBook):
+            ...
+
+
+    bot = CustomBot(exchange, username, password)
+
+    response = requests.get(
+        f"{exchange}/api/trade", headers=bot._get_headers()
+    )
+    trades = response.json()
+    df = pd.DataFrame(trades)
+    df['timestamp'] = pd.to_datetime(df['timestamp'])
+
+    # 2. Define the time window
+    latest_time = df['timestamp'].max()
+    timedelta = 5
+    start_time = latest_time - pd.Timedelta(minutes=timedelta)
+
+    recent_trades = df[df['timestamp'] >= start_time]
+
+    if not recent_trades.empty:
+        # 2. Group by product and sum the volume
+        # .idxmax() returns the index (product name) with the highest value
+        most_traded_product = recent_trades.groupby('product')['volume'].sum().idxmax()
+
+        # Optional: Get the actual volume number too
+        total_vol = recent_trades.groupby('product')['volume'].sum().max()
+
+        print(f"Most traded product: {most_traded_product} (Volume: {total_vol})")
+    else:
+        print("No trades in the timeframe.")
+
+    recent_trades = recent_trades[recent_trades['product'] == most_traded_product]
+
+    spread = get_spread(recent_trades)
+
+    # Calculate VWAP
+    target_product = most_traded_product
+    if not recent_trades.empty:
+        total_value = (recent_trades['price'] * recent_trades['volume']).sum()
+        total_volume = recent_trades['volume'].sum()
+
+        # Avoid division by zero if volume is 0
+        if total_volume > 0:
+            vwap = total_value / total_volume
+            print(f"VWAP for {target_product} (Last {timedelta} Mins): {vwap:.2f}")
+            print(f"Based on {len(recent_trades)} trades from {start_time.time()} to {latest_time.time()}")
+        else:
+            print("Total volume is 0.")
+    else:
+        print(f"No trades found for {target_product} in the last 10 minutes.")
+        return None
+
+    return most_traded_product, vwap, spread
+    
